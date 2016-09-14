ad9164534b5c0ed5aacc922186a44734bd26d416
RiderZhang <1750455690@qq..com>
2016-09-14 13:38:39 +0800

    Add Class

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace SendEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dns.GetHostName();
+            IPAddress[] IP = Dns.GetHostAddresses(Dns.GetHostName());
+            string loginIP = IP[0].ToString();
+            //string dateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            //string dateTimes = DateTime.Now.ToLongTimeString();
+            //SendEmailHelper.Send();
+        }
+        //将图片导入到excel中实例
+        //public void  LoadImage()
+        //{
+        //    //说明：插入图片  
+
+        //    //1.创建EXCEL中的Workbook           
+        //    IWorkbook myworkbook = new HSSFWorkbook();
+
+        //    //2.创建Workbook中的Sheet          
+        //    ISheet mysheet = myworkbook.CreateSheet("sheet1");
+        //    //第一步：读取图片到byte数组  
+        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://img1.soufunimg.com/message/images/card/tuanproj/201511/2015112703584458_s.jpg");
+
+        //    byte[] bytes;
+        //    using (Stream stream = request.GetResponse().GetResponseStream())
+        //    {
+        //        using (MemoryStream mstream = new MemoryStream())
+        //        {
+        //            int count = 0;
+        //            byte[] buffer = new byte[1024];
+        //            int readNum = 0;
+        //            while ((readNum = stream.Read(buffer, 0, 1024)) > 0)
+        //            {
+        //                count = count + readNum;
+        //                mstream.Write(buffer, 0, 1024);
+        //            }
+        //            mstream.Position = 0;
+        //            using (BinaryReader br = new BinaryReader(mstream))
+        //            {
+
+        //                bytes = br.ReadBytes(count);
+        //            }
+        //        }
+        //    }
+
+        //    //第二步：将图片添加到workbook中  指定图片格式 返回图片所在workbook->Picture数组中的索引地址（从1开始）  
+        //    int pictureIdx = myworkbook.AddPicture(bytes, PictureType.JPEG);
+
+        //    //第三步：在sheet中创建画部  
+        //    IDrawing patriarch = mysheet.CreateDrawingPatriarch();
+
+        //    //第四步：设置锚点 （在起始单元格的X坐标0-1023，Y的坐标0-255，在终止单元格的X坐标0-1023，Y的坐标0-255，起始单元格行数，列数，终止单元格行数，列数）  
+        //    IClientAnchor anchor = patriarch.CreateAnchor(0, 0, 0, 0, 0, 0, 2, 2);
+
+
+        //    //第五步：创建图片  
+        //    IPicture pict = patriarch.CreatePicture(anchor, pictureIdx);
+
+        //    //6.保存         
+        //    FileStream file = new FileStream(@"E:\myworkbook11.xls", FileMode.Create);
+        //    myworkbook.Write(file);
+        //    file.Close();
+        //}
+    }
+}
---
+++ SendEmailHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace SendEmail
+{
+    class SendEmailHelper
+    {
+        public static void Send()
+        {
+            string from = "504186331@qq.com";//"m18060796494@163.com";//"rid@noblehousehome.com";
+            string fromer = "发件人";
+            string to = "1750455690@qq.com";
+            string toer = "收件人";
+            string Subject = "邮件标题";
+            string file = @"E:\zxh.xlsx";
+            string Body = "发送excel";
+            string SMTPHost = "smtp.qq.com";// "smtp.exmail.qq.com"; //"smtp.163.com";//  
+            string SMTPuser = "504186331@qq.com";//"m18060796494@163.com";//
+            string SMTPpass = "....";// "zxh@163.com";// "Per796494";
+            Sendmail(from, fromer, to, toer, Subject, Body, file, SMTPHost, SMTPuser, SMTPpass);
+        }
+        /// <summary>
+        /// C#发送邮件函数
+        /// </summary>
+        /// <param name="from">发送者邮箱</param>
+        /// <param name="fromer">发送人</param>
+        /// <param name="to">接受者邮箱</param>
+        /// <param name="toer">收件人</param>
+        /// <param name="Subject">主题</param>
+        /// <param name="Body">内容</param>
+        /// <param name="file">附件</param>
+        /// <param name="SMTPHost">smtp服务器</param>
+        /// <param name="SMTPuser">邮箱</param>
+        /// <param name="SMTPpass">密码</param>
+
+        /// <returns></returns>
+        public static bool Sendmail(string sfrom, string sfromer, string sto, string stoer, string sSubject, string sBody, string sfile, string sSMTPHost, string sSMTPuser, string sSMTPpass)
+        {
+            ////设置from和to地址
+            MailAddress from = new MailAddress(sfrom, sfromer);
+            MailAddress to = new MailAddress(sto, stoer);
+
+            ////创建一个MailMessage对象
+            MailMessage oMail = new MailMessage(from, to);
+
+            //// 添加附件
+            if (sfile != "")
+            {
+                oMail.Attachments.Add(new Attachment(sfile));
+            }
+
+
+
+            ////邮件标题
+            oMail.Subject = sSubject;
+
+
+            ////邮件内容
+            oMail.Body = sBody;
+
+            ////邮件格式
+            oMail.IsBodyHtml = false;
+
+            ////邮件采用的编码
+            oMail.BodyEncoding = System.Text.Encoding.GetEncoding("GB2312");
+
+            ////设置邮件的优先级为高
+            oMail.Priority = MailPriority.High;
+
+            ////发送邮件
+            SmtpClient client = new SmtpClient();
+            ////client.UseDefaultCredentials = false;
+            client.Host = sSMTPHost;
+            client.Credentials = new NetworkCredential(sSMTPuser, sSMTPpass);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            try
+            {
+                client.Send(oMail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                System.Web.HttpContext.Current.Response.Write(ex.Message.ToString());
+                return false;
+            }
+            finally
+            {
+                ////释放资源
+                oMail.Dispose();
+            }
+
+        }
+    }
+}
